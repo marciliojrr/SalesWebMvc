fc17e6d2d5f0b224da697d0db95d6d7b3b0d8891
Marcílio Júnior <marciliojr@outlook.com>
2022-03-31 18:59:25 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,9 +1,36 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         // Props
         public int Id { get; set; }
         public string Name { get; set; }
+
+        // Associacao entre 'Department' e 'Seller' - 1 'Department' tem varios 'Sellers'.
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        // Constructors
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // Methods
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
