40d925e56470dc0f98fefce89fc544290f4ccce7
Marcílio Júnior <marciliojr@outlook.com>
2022-04-01 14:50:20 -0300

    SellerService and basic FindAll()

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll() // Sincrona, por enquanto
+        {
+            return _context.Seller.ToList(); // Acessa a fonte de dados relacionadas a tabela Seller e converte para uma lista
+        }
+    }
+}
